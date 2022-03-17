dc5c142ea9e542e1051216e0cbcda9fe0aaccaa3
darkdevil18 <sampaddibya@gmail.com>
2022-03-17 09:17:17 +0530

    adding files

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from api_key import api_key
+
+app = FastAPI()
+
+origins = [
+	'http://localhost:3000',
+	'https://myweather-app-project.herokuapp.com'
+]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["GET"],
+	allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def weather_api():
+	return {
+		"details": "This api is just a middleware between the actual api and the website"
+	}
+
+
+@app.get("/api/weather/{city}",
+         description='Returns weather details of the city provided')
+async def weather_details(city: str):
+	data = requests.get(
+		f'https://api.openweathermap.org/data/2.5/weather?', {
+			'q': city,
+			'APPID': api_key,
+			'units': 'metric',
+		})
+
+	return data.json()
